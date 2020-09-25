67c69dccbb11b6b13ecc4e7fcbe6ebcd478e66ae
Kyle Corbin <kylecorbin@Kyles-iMac.attlocal.net>
2020-09-24 20:13:18 -0400

    cleaned up the code a good bit

--- src/index.js
+++ src/index.js
@@ -7,17 +7,7 @@ import { registerServiceWorker } from './fakeServiceWorker'
 // import * as serviceWorker from './serviceWorker';
 import store from './Redux/store';
 
-if ('serviceWorker' in navigator) {
-  console.log('Trying to register custom sw');
-  navigator.serviceWorker
-    .register('./firebase-messaging-sw.js')
-    .then(function (registration) {
-      console.log('Registration successful, scope is:', registration.scope);
-    })
-    .catch(function (err) {
-      console.log('Service worker registration failed, error:', err);
-    });
-}
+
 
 ReactDOM.render(
   <Provider store={store}>
